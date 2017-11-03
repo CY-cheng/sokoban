195325c2c038794641ed04385a48aaf2eac2d8c5
CY-cheng <sss621018tw@gmail.com>
2017-11-03 13:51:05 +0800

    2017-11-03 method and url

--- httpd/index.js
+++ httpd/index.js
@@ -3,19 +3,15 @@
 let http = require('http');
 
 http.createServer((request, response) => {
-    // 取得 node.js 的 fs 模組
-    let fs = require('fs')
-
-    fs.readFile('../htdocs/index.html', (err, data) => {
-        response.writeHead(200, {
-        'Content-Type': 'text/html'
-        });
-
-        response.write(data);
-
-        response.end();
+    request.on('end', () => {
+        console.log('Request method: ' + request.method);
+        console.log('Request url: ' + request.url);
     });
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    response.end('Hello World!\n');
 }).listen(8088);
 
-// log message to Console
-console.log(' 伺服器啟動，連線 url: http://127.0.0.1:8088/');
+console.log('Server running at http://127.0.0.1:8088/');
