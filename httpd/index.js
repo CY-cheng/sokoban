80336a4f0708aede19b113b546ce7c3bd4ddc987
CY-cheng <sss621018tw@gmail.com>
2017-11-10 15:15:13 +0800

    updates

--- httpd/index.js
+++ httpd/index.js
@@ -2,21 +2,33 @@
 
 let http = require('http');
 
+const routingTable = {
+    '/': {
+        url: '../htdocs/index.html',
+        type: 'text/html'
+    },
+    '/styles.css': {
+        url: '../htdocs/assets/css/styles.css',
+        type: 'text/css'
+    },
+    '/SokobanClone_byVellidragon.png': {
+        url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
+        type: 'text/css'
+    },
+};
+
 let serve = (response, fname, datatype) => {
-    
     let fs = require('fs');
-    
+
     fs.readFile(fname, (err, data) => {
-        
         if (err) {
             console.log(' 檔案讀取錯誤');
         }
-        
         else {
             response.writeHead(200, {
-                'Content-Type': datatype
+            'Content-Type': datatype
             });
-            
+
             response.write(data);
             response.end();
         }
@@ -27,42 +39,28 @@ http.createServer((request, response) => {
     let fs = require('fs');
 
     let postData = '';
-    
+
     request.on('data', (chunk) => {
-        
         postData += chunk;
 
         console.log(
-            ' 接收的 POST data 片段 k: [' + chunk + '].'
+            ' 接收的 POST data ⽚段 k: [' + chunk + '].'
         );
     });
 
     request.on('end', () => {
-        
-        switch (request.url) {
-            case '/':
-                serve(response, '../htdocs/index.html', 'text/html');
-                break;
-            
-            case '/assets/css/styles.css':
-                serve(response, '../htdocs/assets/css/styles.css', 'text/css');              
-                break;
+        if (request.url in routingTable) {
+            let obj = routingTable[request.url];
 
-            case '/assets/png/SokobanClone_byVellidragon.png':
-                serve(response, '../htdocs/assets/png/SokobanClone_byVellidragon.png', 'image/png');              
-                break;  
-                
-            case '/favicon.ico':
-                serve(response, '../htdocs/assets/ico/favicon.ico', 'image/png');
-                break;
+            serve(response, obj.url, obj.type);
+        }
+        else {
+            console.log(' 未定義的存取: ' + request.url);
 
-            default:
-                console.log(' 未定義的存取: ' + request.url);
-                response.end();
-                break;
+            response.end();
         }
     });
-    
 }).listen(8088);
 
 console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+
