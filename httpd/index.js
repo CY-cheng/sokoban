a03889ba2670afb954a85e60b404a15b89f9a393
CY-cheng <sss621018tw@gmail.com>
2017-11-10 13:55:41 +0800

    將switch程式縮減，使用serve函式做出

--- httpd/index.js
+++ httpd/index.js
@@ -2,86 +2,67 @@
 
 let http = require('http');
 
+let serve = (response, fname, datatype) => {
+    
+    let fs = require('fs');
+    
+    fs.readFile(fname, (err, data) => {
+        
+        if (err) {
+            console.log(' 檔案讀取錯誤');
+        }
+        
+        else {
+            response.writeHead(200, {
+                'Content-Type': datatype
+            });
+            
+            response.write(data);
+            response.end();
+        }
+    });
+};
+
 http.createServer((request, response) => {
     let fs = require('fs');
+
     let postData = '';
     
     request.on('data', (chunk) => {
+        
         postData += chunk;
 
-    console.log(
-    ' 接收的 POST data 片段 k: [' + chunk + '].'
-    );
+        console.log(
+            ' 接收的 POST data 片段 k: [' + chunk + '].'
+        );
     });
 
     request.on('end', () => {
+        
         switch (request.url) {
             case '/':
-                fs.readFile('../htdocs/index.html', (err, data) => {
-                    if (err) {
-                        console.log(' 檔案讀取錯誤');
-                    }
-                    else {
-                        response.writeHead(200, {
-                            'Content-Type': 'text/html'
-                        });
-                    response.write(data);
-                    response.end();
-                    }
-                });
-            break;
+                serve(response, '../htdocs/index.html', 'text/html');
+                break;
             
             case '/assets/css/styles.css':
-                fs.readFile('../htdocs/assets/css/styles.css', (err, data) => {
-                    if (err) {
-                        console.log(' 檔案讀取錯誤');
-                    }
-                    else {
-                        response.writeHead(200, {
-                            'Content-Type': 'text/css'
-                        });
-                    response.write(data);
-                    response.end();
-                    }
-                });
-            break;
-            
+                serve(response, '../htdocs/assets/css/styles.css', 'text/css');              
+                break;
+
             case '/assets/png/SokobanClone_byVellidragon.png':
-                fs.readFile('../htdocs/assets/png/SokobanClone_byVellidragon.png', (err, data) => {
-                    if (err) {
-                        console.log(' 檔案讀取錯誤');
-                    }
-                    else {
-                        response.writeHead(200, {
-                            'Content-Type': 'image/png'
-                        });
-                    response.write(data);
-                    response.end();
-                    }
-                });
-            break;
-            
+                serve(response, '../htdocs/assets/png/SokobanClone_byVellidragon.png', 'image/png');              
+                break;  
+                
             case '/favicon.ico':
-                fs.readFile('../htdocs/favicon.ico', (err, data) => {
-                    if (err) {
-                        console.log(' 檔案讀取錯誤');
-                    }
-                    else {
-                        response.writeHead(200, {
-                            'Content-Type': 'image/png'
-                        });
-                    response.write(data);
-                    response.end();
-                    }
-                });
-            break;
+                serve(response, '../htdocs/assets/ico/favicon.ico', 'image/png');
+                break;
 
             default:
                 console.log(' 未定義的存取: ' + request.url);
                 response.end();
-            break;
+                break;
         }
     });
+    
 }).listen(8088);
 
 console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
